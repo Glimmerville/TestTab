0ae559eb96e35fcd6fecf5d90f078bb4aa30792d
Your Name <you@email.com>
2016-05-24 14:14:36 -0400

    initial commit

---
+++ TabTest/scripts/MyScript.js
@@ -0,0 +1,22 @@
+﻿// This is a shortcut for $(document).ready()
+$(function () {
+
+    // This is a shortcut to bind both mouseOver and mouseOut
+    $('div.slideOutTab').hover(function () {
+        // Animate out when hovered, stopping all previous animations
+        $(this)
+            .stop(true, false)
+            .animate({
+                left: -16
+            }, 350);
+    }, function () {
+        // Animate back in when not hovered, stopping all previous animations
+        $(this)
+            .stop(true, false)
+            .animate({
+                left: -350
+            }, 380);
+    });
+
+
+});
